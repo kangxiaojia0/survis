d39511fb615eb0e2c52f76edba830e8c0162bd62
kangxiaojia0 <59348536+kangxiaojia0@users.noreply.github.com>
2024-05-17 01:05:36 +0100

    Add files via upload

---
+++ data/generated/bib.js
@@ -0,0 +1,123 @@
+﻿define({ entries : {
+    "Beck2016Visual": {
+        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
+        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
+        "doi": "10.1109/TVCG.2015.2467757",
+        "journal": "IEEE Transactions on Visualization and Computer Graphics",
+        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
+        "number": "01",
+        "publisher": "IEEE",
+        "series": "TVCG",
+        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
+        "type": "article",
+        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
+        "volume": "22",
+        "year": "2016"
+    },
+    "chen2017intelligent": {
+        "author": "Chen, Feng and Luo, Feng and Liu, Zhen and Sun, Huajun",
+        "doi": "10.1016/j.future.2017.06.027",
+        "journal": "Future Generation Computer Systems",
+        "pages": "482-490",
+        "title": "An Intelligent System for Securing Industrial IoT Communications",
+        "type": "article",
+        "volume": "76",
+        "year": "2017"
+    },
+    "kavitha2022data": {
+        "author": "Kavitha, T and Pandeeswari, N and Shobana, R and Vinothini, V R and Sakthisudhan, K and Jeyam, A and Jasmine Gnana Malar, A",
+        "doi": "10.1016/j.measen.2022.100563",
+        "journal": "Measurement: Sensors",
+        "pages": "100563",
+        "publisher": "Elsevier",
+        "title": "Data congestion control framework in Wireless Sensor Network in IoT enabled intelligent transportation system",
+        "type": "article",
+        "volume": "24",
+        "year": "2022"
+    },
+    "li2018provably": {
+        "author": "Li, Xiong and Zheng, Xiumin and Choo, Kim-Kwang Raymond and Chen, Jianfeng",
+        "doi": "10.1109/TITS.2018.2791450",
+        "journal": "IEEE Transactions on Intelligent Transportation Systems",
+        "number": "3",
+        "pages": "703-714",
+        "title": "A Provably Secure Mobile User Authentication Scheme for Big Data Collection in IoT-Enabled Maritime Intelligent Transportation System",
+        "type": "article",
+        "volume": "19",
+        "year": "2018"
+    },
+    "pitchai2021intelligent": {
+        "author": "Pitchai, Mohana Priya and Ramachandran, Manikandan and Al-Turjman, Fadi and Mostarda, Leonardo",
+        "doi": "10.32604/cmc.2021.015568",
+        "journal": "Computers, Materials \\& Continua",
+        "number": "3",
+        "pages": "5617-5634",
+        "title": "Intelligent Framework for Secure Transportation Systems Using Software-Defined-Internet of Vehicles",
+        "type": "article",
+        "volume": "70",
+        "year": "2021"
+    },
+    "qu2020iot": {
+        "author": "Qu, Jing and Sun, Lina and Ma, Yuan",
+        "doi": "10.1007/s12652-019-01638-w",
+        "journal": "Journal of Ambient Intelligence and Humanized Computing",
+        "pages": "4009-4020",
+        "title": "IoT-based smart rehabilitation system",
+        "type": "article",
+        "volume": "11",
+        "year": "2020"
+    },
+    "rajaguru2021intelligent": {
+        "author": "Rajaguru, R and Mathankumar, M and Balakumar, A and Dinesh Kumar, J R and Alexander Stonier, Albert",
+        "doi": "10.1088/1757-899X/1055/1/012127",
+        "journal": "IOP Conference Series: Materials Science and Engineering",
+        "pages": "012127",
+        "publisher": "IOP Publishing",
+        "title": "An Intelligent Framework for Traffic Management System Using Narrow Band-IoT",
+        "type": "article",
+        "volume": "1055",
+        "year": "2021"
+    },
+    "rao2019enhanced": {
+        "author": "Rao, Madhusudan and Prasad, Ramakrishna",
+        "doi": "10.1016/j.compeleceng.2019.102200",
+        "journal": "Computers \\& Electrical Engineering",
+        "pages": "102200",
+        "title": "Enhanced Privacy and Security in IoT-based Wearable Health Monitoring Systems",
+        "type": "article",
+        "volume": "79",
+        "year": "2019"
+    },
+    "sun2017toward": {
+        "author": "Sun, Xiaowei and Luo, Hongmei and Peng, Ling and Li, Xianwei",
+        "doi": "10.1109/JIOT.2017.2753918",
+        "journal": "IEEE Internet of Things Journal",
+        "number": "6",
+        "pages": "2236-2252",
+        "title": "Toward Location-Enabled IoT (LE-IoT): IoT Positioning Techniques, Error Sources, and Error Mitigation",
+        "type": "article",
+        "volume": "4",
+        "year": "2017"
+    },
+    "wang2020survey": {
+        "author": "Wang, Yun and Li, Jie and Ren, Kan and Yin, Yizhou",
+        "doi": "10.1016/j.future.2019.05.019",
+        "journal": "Future Generation Computer Systems",
+        "pages": "490-500",
+        "title": "A Survey of Big Data Security in Cloud Computing",
+        "type": "article",
+        "volume": "99",
+        "year": "2020"
+    },
+    "zhang2019designing": {
+        "author": "Zhang, Y and Zhang, K and Zheng, D and Choo, Kim-Kwang Raymond and Zhang, Y",
+        "doi": "10.1109/TII.2019.2926117",
+        "journal": "IEEE Transactions on Industrial Informatics",
+        "number": "10",
+        "pages": "5553-5562",
+        "title": "Designing Secure User Authentication Protocol for Big Data Collection in IoT-Based Intelligent Transportation System",
+        "type": "article",
+        "volume": "15",
+        "year": "2019"
+    }
+}});
